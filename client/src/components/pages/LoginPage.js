0bfb67391938667090dc2b13fb63cfb8200a6ff9
etanaris <etanarisy@dhcp-18-20-164-34.dyn.mit.edu>
2020-01-15 11:48:04 -0500

    updated homepage

---
+++ client/src/components/pages/LoginPage.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "./LoginPage.css";
+
+
+const GOOGLE_CLIENT_ID = "529894289409-jecjp2cbbu9hsu3fobsrc063mv19t99r.apps.googleusercontent.com";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // remember -- api calls go here!
+  }
+
+  render() {
+    if (!this.state.user) {
+      return (
+        <>
+          <div className="LoginPage-container">
+            <div className="footer">
+              <h1 className="LoginPage-title"> study garden </h1>
+              {this.props.userId ? (
+                <GoogleLogout
+                  clientId={GOOGLE_CLIENT_ID}
+                  buttonText="Logout"
+                  onLogoutSuccess={this.props.handleLogout}
+                  onFailure={(err) => console.log(err)}
+                />
+              ) : (
+                <GoogleLogin
+                  clientId={GOOGLE_CLIENT_ID}
+                  buttonText="Login"
+                  onSuccess={this.props.handleLogin}
+                  onFailure={(err) => console.log(err)}
+                />
+              )}
+            <img src="https://i.imgur.com/peJQgf0.png"/>
+            </div>
+            <br/>
+          </div>
+
+        </>
+      );
+    }
+  }
+}
+
+export default LoginPage;
