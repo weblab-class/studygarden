aa55fd4800955437032d4b45f81e0e26baf46b2a
etanaris <etanarisy@dhcp-18-21-137-75.dyn.mit.edu>
2020-01-15 12:13:16 -0500

    changed the login page

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -23,25 +23,30 @@ class LoginPage extends Component {
       return (
         <>
           <div className="LoginPage-container">
-            <h1 className="LoginPage-title"> study garden </h1>
-            {this.props.userId ? (
-              <GoogleLogout
-                clientId={GOOGLE_CLIENT_ID}
-                buttonText="Logout"
-                onLogoutSuccess={this.props.handleLogout}
-                onFailure={(err) => console.log(err)}
-                className="NavBar-link NavBar-login"
-              />
-            ) : (
-              <GoogleLogin
-                clientId={GOOGLE_CLIENT_ID}
-                buttonText="Login"
-                onSuccess={this.props.handleLogin}
-                onFailure={(err) => console.log(err)}
-                className="NavBar-link NavBar-login"
-              />
-            )}
+            <div className="footer">
+              <h1 className="LoginPage-title"> study garden </h1>
+              {this.props.userId ? (
+                <GoogleLogout
+                  clientId={GOOGLE_CLIENT_ID}
+                  buttonText="Logout"
+                  onLogoutSuccess={this.props.handleLogout}
+                  onFailure={(err) => console.log(err)}
+                  className="NavBar-link NavBar-login"
+                />
+              ) : (
+                <GoogleLogin
+                  clientId={GOOGLE_CLIENT_ID}
+                  buttonText="Login"
+                  onSuccess={this.props.handleLogin}
+                  onFailure={(err) => console.log(err)}
+                  className="NavBar-link NavBar-login"
+                />
+              )}
+            <img src="https://i.imgur.com/peJQgf0.png"/>
+            </div>
+            <br/>
           </div>
+
         </>
       );
     }
