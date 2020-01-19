c5ae95c9de0b96c71870b350557bcb484f70a23a
Tommy Adebiyi <13374036+tomadto@users.noreply.github.com>
2020-01-19 02:30:02 -0500

    t i m i n g

--- server/models/plant.js
+++ server/models/plant.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const PlantSchema = new mongoose.Schema({
@@ -11,12 +11,13 @@ const PlantSchema = new mongoose.Schema({
   studyTimeCumul: Number,
   stage: Number,
   isStudying: Boolean,
-  homePageIndex: Number,
-  /* in case we want to let people reorder their plants 
-  (doesn't seem too hard to implement)*/
-  // famous last words
+  // homePageIndex: Number,
+  // /* in case we want to let people reorder their plants
+  // (doesn't seem too hard to implement)*/
+  // // famous last words
+  //  actually is redundant, oops
   pomodoroCount: { type: Number, default: 0 },
 });
 
 // compile model from schema
-module.exports = mongoose.model("plant", PlantSchema);
+module.exports = mongoose.model('plant', PlantSchema);
