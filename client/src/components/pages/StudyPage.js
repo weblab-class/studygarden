3ebf78f27c454dc51a5b3198f983dd13620f5583
Tommy Adebiyi <13374036+tomadto@users.noreply.github.com>
2020-01-22 14:33:33 -0500

    timer now reports correct time at beginning of session

--- client/src/components/pages/StudyPage.js
+++ client/src/components/pages/StudyPage.js
@@ -46,7 +46,9 @@ class StudyPage extends Component {
     // );
     //let a = await sessionTimer.tick()
     //console.log(a+"b")
-
+    this.setState({
+      timeString: this.convertToMinSec(sesLength),
+    });
     this.nMiniDaemon = new MiniDaemon(
       this,
       (index, length, backwards) => {
@@ -56,10 +58,12 @@ class StudyPage extends Component {
       1000,
       sesLength
     );
+
     this.setState({
       isStudying: true,
       sessionLength: sesLength,
     });
+
     this.nMiniDaemon.start();
     post(`/api/session/update`, {
       creatorId: this.props.userId,
