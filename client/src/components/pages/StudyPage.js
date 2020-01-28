14cfbbf671906d4f5abd4b2d3f6674406666c1ce
Tommy Adebiyi <13374036+tomadto@users.noreply.github.com>
2020-01-28 13:05:01 -0500

    uncomment

--- client/src/components/pages/StudyPage.js
+++ client/src/components/pages/StudyPage.js
@@ -387,11 +387,11 @@ class StudyPage extends Component {
                 }}
               />
           <div className="StudyPage-container">
-            {/* <div class="cloud x1"></div>
+            <div class="cloud x1"></div>
             <div class="cloud x2"></div>
             <div class="cloud x3"></div>
             <div class="cloud x4"></div>
-            <div class="cloud x5"></div> */}
+            <div class="cloud x5"></div>
             {this.state.user && this.state.plant ? (
               <>
              
